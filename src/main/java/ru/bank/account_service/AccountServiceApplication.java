6a33212d3611f4b06f932e8eb80a96a5c936f3bd
Иван Чухманов <oollvanolloo@yandex.ru>
2026-08-17 10:56:44 +0300

    feat: Старт Account-Service, будет отвечать за счета, балансы, типы счетов

---
+++ src/main/java/ru/bank/account_service/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.bank.account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
