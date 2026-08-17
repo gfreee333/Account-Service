2ec725c994caac680a1423a0f4b206631a7e4a7c
Иван Чухманов <oollvanolloo@yandex.ru>
2026-08-17 11:28:42 +0300

    feat: Добавил основную конфигурацию - Подключение к Config-Service - Подключение к Discovery-Service

--- src/main/java/ru/bank/account_service/AccountServiceApplication.java
+++ src/main/java/ru/bank/account_service/AccountServiceApplication.java
@@ -2,12 +2,12 @@ package ru.bank.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
-
 }
